66827892d1d17506c38fd80567284a78fe924d0e
Miguel <miguelpencar@gmail.com>
2023-05-03 01:15:09 +0200

    Minor change in IO_model.py

--- src/IO_model.py
+++ src/IO_model.py
@@ -5,11 +5,11 @@ PATH = 'trainedModel/'
 
 def save_model(model, dataset, inverted=False):
     model_scripted = torch.jit.script(model)  # Export to TorchScript
-    model_scripted.save(PATH + dataset[2] + ('inverted' if inverted else '') + '.pt')  # Save
+    model_scripted.save(PATH + dataset[2] + ('_inverted' if inverted else '') + '.pt')  # Save
 
 
 def load_model(dataset, inverted=False):
-    model = torch.jit.load(PATH + dataset[2] + ('inverted' if inverted else '') + '.pt')
+    model = torch.jit.load(PATH + dataset[2] + ('_inverted' if inverted else '') + '.pt')
     model.eval()
 
     return model
